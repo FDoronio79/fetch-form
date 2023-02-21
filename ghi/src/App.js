affcc1465497d2851b925cbcdf32be2870e618a9
Filamer Doronio <filamerdoronio@gmail.com>
2023-02-21 10:12:17 -0800

    testing

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ?????.
+          PLS GIVE ME A JOB
         </p>
         <a
           className="App-link"
